dff1bf295322e4e62d0021ac9f8a9d1418e5f700
m.dospina <m.dospina@l2f.ch>
2020-01-08 16:01:12 +0100

    refactor code

--- env/StockTradingEnv.py
+++ env/StockTradingEnv.py
@@ -66,8 +66,6 @@ class StockTradingEnv(gym.Env):
                     if item[0] != self.i:
                         item_price = item[3]
                         if movement_price >= item_price and self.balance >= item[3]*item[2] and find:
-                           # print('TROVATO ASK: ')
-                           # print(action_type)
                             shares_bought = item[2]
                             prev_cost = self.cost_basis * self.shares_held
                             additional_cost = item_price
@@ -81,7 +79,6 @@ class StockTradingEnv(gym.Env):
                             break
                     j = j + 1
         elif action_type> 1 and action_type <= 2:
-           ## print('VENDO')
             if self.bids.size > 0:
                 j = 0
                 for item in self.bids:
@@ -89,9 +86,6 @@ class StockTradingEnv(gym.Env):
                         item_price = item[3]
                         shares_sold = item[2]
                         if movement_price <= item_price and self.shares_held >= shares_sold and find:
-                            #print('TROVATO BIDS: ')
-                            #print('bid:')
-                            #print(item)
                             self.balance += item_price
                             self.shares_held -= shares_sold
                             self.total_shares_sold += shares_sold
@@ -119,8 +113,6 @@ class StockTradingEnv(gym.Env):
 
     def step(self, action):
         # Execute one time step within the environment
-        #print('action')
-        #print(action)
         self._take_action(action)
         self.current_step += 1
         if self.current_step > 5242:
@@ -161,26 +153,12 @@ class StockTradingEnv(gym.Env):
             f'Net worth: {self.net_worth} (Max net worth: {self.max_net_worth})')
         print(f'Profit: {profit}')
     def step_wrapper(self, action, price, i, bids, asks, transaction, current_step):
-        #print('i: ', i)
-
         self.current_step = current_step
-
         self.bids = np.array(bids)
-
         self.i = i
-
         self.asks = np.array(asks)
-
         self.current_price = price
-
-
-
         self.transaction = transaction
-        j = 0
-        #print('transaction size: ')
-        #print(self.transaction.size)
-        #print('transaction')
-        #print(self.transaction)
         j = 0
         for item in self.transaction:
             agent_id = item[0]
@@ -188,27 +166,14 @@ class StockTradingEnv(gym.Env):
             movement_price = item[3]
             shares = item[2]
             if agent_id == self.i:
-                #print('entrato')
                 if action_type <= 1:
                     self.transaction = np.delete(self.transaction,[j], 0)
                     self.shares_held += shares
-
                 elif action_type > 1 and action_type <= 2:
                     self.total_shares_sold += shares
                     self.balance += movement_price*shares
                     self.transaction = np.delete(self.transaction, [j], 0)
 
             j += 1
-
-        #print('_______________________________')
-        #print('prima price')
-        #print(self.current_price)
         obs, rew, done, info = self.step(action)
-        #print('dopo price')
-        #print(self.current_price)
-        #print('balance')
-        #print(self.balance)
-        #print('_______________________________')
-
-
         return obs, rew, done, info, self.bids, self.asks, self.current_price, self.transaction
