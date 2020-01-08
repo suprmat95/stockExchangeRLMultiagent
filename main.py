031841967cda40f10c9d984a694e6dcbeacb5276
m.dospina <m.dospina@l2f.ch>
2020-01-08 17:53:04 +0100

    refactor code

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def select_policy(agent_id):
 #print(asks)
 #asks = np.delete(asks, [1], axis=0)
 #print(asks)
-#ray.init()
+ray.init()
 
 
 register_env("test", lambda _: StockTradingMultiAgent(2))
@@ -49,16 +49,15 @@ tune.run(
             "eager": False,
             "simple_optimizer": True,
             "multiagent": {
-                  "policies": {
-                                   "pg_policy": (None,  spaces.Box(
-             low=0, high=1, shape=(1, 6), dtype=np.float16),  spaces.Box(
-            low=np.array([0, 0, -1]), high=np.array([3, 0.2, 1]), dtype=np.float16), {}),
-                                   "random": (None,  spaces.Box(
-             low=0, high=1, shape=(1, 6), dtype=np.float16),  spaces.Box(
-            low=np.array([0, 0, -1]), high=np.array([3, 0.2, 1]), dtype=np.float16), {}),
-                               },
-                "policy_mapping_fn": (
-                    lambda agent_id: ["pg_policy", "random"][agent_id % 2]),
+                "policies": {
+                                "pg_policy": (None,  spaces.Box(
+                                    low=0, high=1, shape=(1, 6), dtype=np.float16),  spaces.Box(
+                                    low=np.array([0, 0, -1]), high=np.array([3, 0.2, 1]), dtype=np.float16), {}),
+                                "random": (None,  spaces.Box(
+                                    low=0, high=1, shape=(1, 6), dtype=np.float16),  spaces.Box(
+                                    low=np.array([0, 0, -1]), high=np.array([3, 0.2, 1]), dtype=np.float16), {}),
+                              },
+                "policy_mapping_fn": (lambda agent_id: ["pg_policy", "random"][agent_id % 2]),
                 "policies_to_train": ["pg_policy","random"],
 
             },
